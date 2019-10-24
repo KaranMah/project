8a29359006b8b933da038a08663d51571e18f87c
astronights <shubhankar.a31@gmail.com>
2019-10-24 17:48:05 +0800

    Financial data scraped 24/10/19

--- num_data.py
+++ num_data.py
@@ -6,7 +6,9 @@ import datetime as datetime
 
 config = {
     "raw_data": "./currencies.txt",
-    "cur_pairs": "./cur_pairs.txt"
+    "cur_pairs": "./cur_pairs.txt",
+    "earliest_date" : '01/01/2010',
+    "latest_date": datetime.datetime.now().strftime("%d/%m/%Y")
 }
 
 def read_file(filename):
@@ -16,23 +18,36 @@ def read_file(filename):
 
 
 def get_currency_pairs(currencies):
-    lol = [(x + "/" + currencies[0]) for x in currencies[1:]]
+    lol = [(currencies[0] + "/" + x) for x in currencies[1:]]
     with open('cur_pairs.txt', 'w') as f:
         for item in lol:
             f.write("%s\n" % item)
 
 
 def get_forex_rates(filename):
-    earliest_date = '01/01/2010'
-    latest_date = datetime.datetime.now().strftime("%d/%m/%Y")
+
     pairs = list(pd.read_csv(filename, header=None)[0])
+    final_df = pd.DataFrame()
     for pair in pairs:
-        df = investpy.get_currency_cross_historical_data(currency_cross='EUR/USD',
-                                                 from_date=earliest_date,
-                                                 to_date=latest_date)
-        print(df.shape)
+        df = investpy.get_currency_cross_historical_data(currency_cross=pair,
+                                                 from_date=config['earliest_date'],
+                                                 to_date=config['latest_date'])
+        final_df = final_df.append(df)
+    final_df.to_csv("historical_forex.csv")
+
+def get_index_rates(data):
+    final_df = pd.DataFrame()
+    for _, row in data.iterrows():
+        for idx in row.Index:
+            df = investpy.get_index_historical_data(index=idx,
+                                        country=row.Country,
+                                        from_date=config['earliest_date'],
+                                        to_date=config['latest_date'])
+            final_df = final_df.append(df)
+    final_df.to_csv("historical_index.csv")
 
 # if __name__ == '__main__':
 data = read_file(config["raw_data"])
-get_currency_pairs(list(data["Currency"]))
-get_forex_rates(config["cur_pairs"])
+# get_currency_pairs(list(data["Currency"]))
+# get_forex_rates(config["cur_pairs"])
+get_index_rates(data[['Country', 'Index']])
