d7144f88d25a39d950c051a6882227e7088586db
astronights <shubhankar.a31@gmail.com>
2019-10-24 17:26:46 +0800

    God knows how many commits

--- num_data.py
+++ num_data.py
@@ -2,10 +2,11 @@ import json
 import investpy
 import numpy as np
 import pandas as pd
-
+import datetime as datetime
 
 config = {
-    "filepath": "./currencies.txt"
+    "raw_data": "./currencies.txt",
+    "cur_pairs": "./cur_pairs.txt"
 }
 
 def read_file(filename):
@@ -14,10 +15,24 @@ def read_file(filename):
     return dataset
 
 
-def get currency_pairs(currencies):
-    ## TODO:
-    data = investpy.get_currency_cross_recent_data(currency_cross='EUR/USD')
+def get_currency_pairs(currencies):
+    lol = [(x + "/" + currencies[0]) for x in currencies[1:]]
+    with open('cur_pairs.txt', 'w') as f:
+        for item in lol:
+            f.write("%s\n" % item)
+
+
+def get_forex_rates(filename):
+    earliest_date = '01/01/2010'
+    latest_date = datetime.datetime.now().strftime("%d/%m/%Y")
+    pairs = list(pd.read_csv(filename, header=None)[0])
+    for pair in pairs:
+        df = investpy.get_currency_cross_historical_data(currency_cross='EUR/USD',
+                                                 from_date=earliest_date,
+                                                 to_date=latest_date)
+        print(df.shape)
 
 # if __name__ == '__main__':
-data = read_file(config["filepath"])
-cur_list = list(data["Currency"])
+data = read_file(config["raw_data"])
+get_currency_pairs(list(data["Currency"]))
+get_forex_rates(config["cur_pairs"])
