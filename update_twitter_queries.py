46e8f414819464ba520f92b1ca5b33db12e47844
astronights <shubhankar.a31@gmail.com>
2019-11-26 18:00:44 +0800

    Some nights I stay up cashing in my bad luck.

--- update_twitter_queries.py
+++ update_twitter_queries.py
@@ -23,9 +23,9 @@ for query in queries:
     print()
     if(os.path.exists("TweetScraper-master/Data/tweet/"+query_file)):
         out = os.popen('tail '+"TweetScraper-master/Data/tweet/"+query_file+' -c 200')
-        oldest = re.search(r"[0-9]{4}-[0-9]{2}-[0-9]{2}", out)
+        oldest = re.search(r"[0-9]{4}-[0-9]{2}-[0-9]{2}", out.read()).group()
         if(datetime.strptime(oldest, "%Y-%m-%d") > datetime.strptime(from_date, "%Y-%m-%d")):
-            new_q = query.split(':')[:-1] + ":" + datetime.strftime(datetime.strptime(oldest, "%Y-%m-%d")-timedelta(days=1), "%Y-%m-%d")
+            new_q = ':'.join(query.split(':')[:-1]) + ":" + datetime.strftime(datetime.strptime(oldest, "%Y-%m-%d")-timedelta(days=1), "%Y-%m-%d")
             new_queries.append(new_q)
         if(datetime.strptime(query.split(':')[-1], "%Y-%m-%d") < datetime.strptime(to_date, "%Y-%m-%d")):
             new_q = query.split(':')[0] + ":" + datetime.strftime(datetime.strptime(query.split(':')[-1], "%Y-%m-%d")+timedelta(days=1), "%Y-%m-%d") + " until:" +  datetime.strftime(datetime.strptime(today, "%Y-%m-%d")-timedelta(days=1), "%Y-%m-%d")
