b04295df0a2b14eef73cfd6a3578d1edba8f0659
astronights <shubhankar.a31@gmail.com>
2019-11-26 18:31:31 +0800

    It's the time to push-code. Kaun mile dekho kisko

--- update_twitter_queries.py
+++ update_twitter_queries.py
@@ -18,22 +18,30 @@ queries = [x.strip() for x in queries]
 new_queries = []
 for query in queries:
     query_file = '_'.join(query.replace(':', ' ').split(' '))
-    print(query)
-    print(query_file)
-    print()
     if(os.path.exists("TweetScraper-master/Data/tweet/"+query_file)):
         out = os.popen('tail '+"TweetScraper-master/Data/tweet/"+query_file+' -c 200')
         oldest = re.search(r"[0-9]{4}-[0-9]{2}-[0-9]{2}", out.read()).group()
         if(datetime.strptime(oldest, "%Y-%m-%d") > datetime.strptime(from_date, "%Y-%m-%d")):
             new_q = ':'.join(query.split(':')[:-1]) + ":" + datetime.strftime(datetime.strptime(oldest, "%Y-%m-%d")-timedelta(days=1), "%Y-%m-%d")
             new_queries.append(new_q)
-        if(datetime.strptime(query.split(':')[-1], "%Y-%m-%d") < datetime.strptime(to_date, "%Y-%m-%d")):
+        if(datetime.strptime(query.split(':')[-1], "%Y-%m-%d") < (datetime.strptime(to_date, "%Y-%m-%d")-timedelta(days=1))):
+            new_q = query.split(':')[0] + ":" + datetime.strftime(datetime.strptime(query.split(':')[-1], "%Y-%m-%d")+timedelta(days=1), "%Y-%m-%d") + " until:" +  datetime.strftime(datetime.strptime(today, "%Y-%m-%d")-timedelta(days=1), "%Y-%m-%d")
+            new_queries.append(new_q)
+    elif(os.path.exists("/data/"+query_file)):
+        out = os.popen('tail '+"/data/"+query_file+' -c 200')
+        oldest = re.search(r"[0-9]{4}-[0-9]{2}-[0-9]{2}", out.read()).group()
+        if(datetime.strptime(oldest, "%Y-%m-%d") > datetime.strptime(from_date, "%Y-%m-%d")):
+            new_q = ':'.join(query.split(':')[:-1]) + ":" + datetime.strftime(datetime.strptime(oldest, "%Y-%m-%d")-timedelta(days=1), "%Y-%m-%d")
+            new_queries.append(new_q)
+        if(datetime.strptime(query.split(':')[-1], "%Y-%m-%d") < (datetime.strptime(to_date, "%Y-%m-%d")-timedelta(days=1))):
             new_q = query.split(':')[0] + ":" + datetime.strftime(datetime.strptime(query.split(':')[-1], "%Y-%m-%d")+timedelta(days=1), "%Y-%m-%d") + " until:" +  datetime.strftime(datetime.strptime(today, "%Y-%m-%d")-timedelta(days=1), "%Y-%m-%d")
             new_queries.append(new_q)
     else:
-        pass
-        new_queries.append(query)
-print(new_queries)
+        if(datetime.strptime(query.split(':')[-1], "%Y-%m-%d") < (datetime.strptime(to_date, "%Y-%m-%d")-timedelta(days=1))):
+            new_q = query.split(':')[0] + ":" + datetime.strftime(datetime.strptime(query.split(':')[-1], "%Y-%m-%d")+timedelta(days=1), "%Y-%m-%d") + " until:" +  datetime.strftime(datetime.strptime(today, "%Y-%m-%d")-timedelta(days=1), "%Y-%m-%d")
+            new_queries.append(new_q)
+        else:
+            new_queries.append(query)
 
 with open('twitter_query_v2.txt', 'w') as f:
     for query in new_queries:
