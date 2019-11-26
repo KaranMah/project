63561831cd80ebf56b70cae21e854f4717478071
astronights <shubhankar.a31@gmail.com>
2019-11-26 21:02:48 +0800

    Pls halp

--- move_scrapy_results.py
+++ move_scrapy_results.py
@@ -7,10 +7,12 @@ from datetime import datetime, timedelta
 
 parser = argparse.ArgumentParser(description='Script to move Scrapy results')
 parser.add_argument('--source', action="store", default="TweetScraper-master/Data/tweet", dest="source", help="Enter directory of files")
-parser.add_argument('--destination', action="store", default="TweetScraper-master", dest="dest", help="Enter directory to move files")
+parser.add_argument('--dest', action="store", default="TweetScraper-master", dest="dest", help="Enter directory containing nohup files")
 source = parser.parse_args().source
 dest = parser.parse_args().dest
-
-files = os.popen('ls '+dest+' | grep -E "nohup"').read()
-queries = [x[6:-4] for x in files]
+print(dest)
+fileobj = os.popen('ls '+dest+' | grep -E "nohup"')
+files = fileobj.read()
+print(files)
+queries = [x[5:-4] for x in files.strip('\n').split('\n')]
 print(queries)
