d9a0288e929684434f8db69e1b1096dc26817192
karanmah <karan22.mahajan@gmail.com>
2019-12-04 20:02:04 +0800

    Made one script. Idk if works perfectly.

---
+++ data_to_json.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+files=`find . -maxdepth 1 -name "#*"`
+for file in $files 
+do
+	echo $file
+	start_year=`echo $file | cut -d'_' -f 3 | cut -d'-' -f 1`
+	end_year=`echo $file | cut -d'_' -f 5 | cut -d'-' -f 1`
+	start_year=$((start_year+1-1))
+	end_year=$((end_year+1-1))
+	for i in $(seq 2010 2019)
+	do
+		echo $i
+		jq '.' $file | jq -s ". | .[] | select((.datetime | contains(\"$i\")) and .is_retweet==false and .is_reply==false) |  {query: .query, username: .usernameTweet, id: .ID, text: .text, nbr_retweet: .nbr_retweet, nbr_reply: .nbr_reply, nbr_favorite: .nbr_favorite, datetime: .datetime}" | jq -s . > "/data/json/$file?${i}.csv"
+	done
+done
