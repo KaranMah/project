4df58568ab32fde4d586ca2deb8956c1b365932a
karanmah <karan22.mahajan@gmail.com>
2019-12-05 22:38:03 +0800

    Finally stopped taking Vietnam from here

---
+++ README.md
@@ -0,0 +1,16 @@
+# Financial Data Forecaster - Repository
+
+Code repository for final year project of building a financial data forecaster
+
+## Relevant Code Files:
+
+-run_query.py
+-move_scrapy_results.py
+-update_twitter_queries.py
+-data_to_json.sh
+
+## Relevant Text Files:
+
+-twitter_query.txt
+-currencies.txt
+
--- data_to_json.sh
+++ data_to_json.sh
@@ -7,9 +7,9 @@ do
 	end_year=`echo $file | cut -d'_' -f 5 | cut -d'-' -f 1`
 	start_year=$((start_year+1-1))
 	end_year=$((end_year+1-1))
-	for i in $(seq 2010 2019)
+	for i in $(seq $start_year $end_year)
 	do
 		echo $i
-		jq '.' $file | jq -s ". | .[] | select((.datetime | contains(\"$i\")) and .is_retweet==false and .is_reply==false) |  {query: .query, username: .usernameTweet, id: .ID, text: .text, nbr_retweet: .nbr_retweet, nbr_reply: .nbr_reply, nbr_favorite: .nbr_favorite, datetime: .datetime}" | jq -s . > "/data/json/$file?${i}.csv"
+		jq '.' $file | jq -s ".[] | select((.datetime | contains(\"$i\")) and .is_retweet==false and .is_reply==false) |  {query: .query, username: .usernameTweet, id: .ID, text: .text, nbr_retweet: .nbr_retweet, nbr_reply: .nbr_reply, nbr_favorite: .nbr_favorite, datetime: .datetime}" | jq -s . > "/data/json/$file?${i}.csv"
 	done
 done
