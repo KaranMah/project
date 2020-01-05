05f331251060400244fcc436d059c8eb056c20ab
astronights <shubhankar.a31@gmail.com>
2020-01-06 02:49:30 +0800

    Doing ML research

--- explore.py
+++ explore.py
@@ -1,4 +1,5 @@
 import json
+import pprint
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -9,6 +10,7 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.ensemble import RandomForestRegressor
 
 def read_forex(filename="historical_forex.csv"):
     df = pd.read_csv(filename, parse_dates=["Date"])
@@ -39,6 +41,8 @@ open_forex[open_forex.isna().any(axis=1)]
 len(open_forex)
 open_forex.count()
 
+open_index.count()
+
 plt.plot(open_forex.iloc[:,4])
 
 ax = sns.heatmap(
@@ -47,27 +51,37 @@ ax = sns.heatmap(
     cmap=sns.diverging_palette(20, 220, n=200),
     square=True)
 
+def plot_results(y_true, y_pred, model):
+    plot_df = pd.concat([y_true.reset_index(drop=True), pd.Series(y_pred)], axis=1, ignore_index=True)
+    plt.figure()
+    plt.plot(plot_df)
+    plt.title(model().__class__.__name__)
+
 def run_sklearn_model(model, train, test, features, target):
     try:
         X_train, y_train = train
         X_test, y_test = test
     except:
         pass
-    reg = model(max_iter=1000, tol=1e-3)
+    reg = model()#max_iter=1000, tol=1e-3)
     reg.fit(X_train, y_train)
+    try:
+        pprint.pprint(dict(zip(features,reg.feature_importances_)))
+    except:
+        pass
     y_pred = reg.predict(X_test)
     print("MSE: ", mean_squared_error(y_test, y_pred))
     print("R2: ", r2_score(y_test, y_pred))
+    plot_results(y_test, y_pred, model)
 
 
-def do_stuff_forex(raw_data, cur, periods):
+def do_stuff_forex(raw_data, cur, periods, model):
     data = raw_data[cur]
     data = data.reset_index()
     data['ret'] = (data[[cur]] - data[[cur]].shift(1))/(data[[cur]].shift(1))
     for i in periods:
             data['ret_'+str(i)] = data['ret'].rolling(i).mean().shift()
     data = data.iloc[max(periods)+1:,:]
-    print(data)
     plt.plot(data['ret'])
     plot_acf(data['ret'])
     plot_pacf(data['ret'])
@@ -77,6 +91,6 @@ def do_stuff_forex(raw_data, cur, periods):
     print("Lag: ", adf_res[2])
     features = list(map(lambda x: "ret_"+str(x), periods))
     X_train, X_test, y_train, y_test = train_test_split(data[features], data['ret'], test_size=0.33)
-    run_sklearn_model(linear_model.SGDRegressor, (X_train, y_train), (X_test, y_test), features, 'ret')
+    run_sklearn_model(model, (X_train, y_train), (X_test, y_test), features, 'ret')
 
-do_stuff_forex(open_forex, "INR", [1, 3, 30])
+do_stuff_forex(open_forex, "INR", [1, 3, 30], RandomForestRegressor)
