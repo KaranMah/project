44df429d2e86f713e57246f4245a353a09301b1e
karanmah <karan22.mahajan@gmail.com>
2020-02-01 19:42:00 +0800

    Just committing to pull

--- json_to_bert-csv.sh
+++ json_to_bert-csv.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-files=`find . -maxdepth 1 -name "#*.json"`
+files=`find /data/json/ -maxdepth 1 -name "#Australia_since_2014-06*.json"`
 for file in $files 
 do
 	echo $file
-	jq -r '.[] | [.text] | @csv' $file > /data/csv/${file/.json/.csv}
+	temp_file=`echo $file | cut -d'/' -f 3`
+	jq -r '.[] | [.text] | @csv' $file > /data/csv/${temp_file/.json/.csv}
 done
