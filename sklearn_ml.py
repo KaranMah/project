41fecc82085d406fd9b7b369317f50cda2043d68
astronights <shubhankar.a31@gmail.com>
2020-02-07 20:49:29 +0800

    Initial crappy linear model on Forex

--- sklearn_ml.py
+++ sklearn_ml.py
@@ -1,17 +1,28 @@
+import json
 import pprint
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn import linear_model
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import *
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error,r2_score
 
 forex = pd.read_csv('prep_forex.csv', header=[0,1], index_col=0)
 index = pd.read_csv('prep_index.csv', header=[0,1,2], index_col=0)
 
+forex_pairs = list(set([x[1] for x in forex.columns if x[0] == 'Close']))
+index_pairs = list(set([(x[1], x[2]) for x in index.columns if x[0] == 'Close']))
+
+reg_models = [LinearRegression, Ridge, RidgeCV, Lasso, LassoCV, LassoLarsCV, LassoLarsIC, MultiTaskLasso,
+          ElasticNet, ElasticNetCV, MultiTaskElasticNet, MultiTaskElasticNetCV, LassoLars,
+          OrthogonalMatchingPursuit, BayesianRidge, ARDRegression,  LogisticRegression, LogisticRegressionCV,
+          SGDRegressor, PassiveAggressiveRegressor, HuberRegressor, RANSACRegressor, TheilSenRegressor]
+
 metric = 'Close'
+metrics = ['Open', 'Close', 'Low', 'High']
 target = [metric+'_Ret']
 features = ['Intraday_HL', 'Intraday_OC', 'Prev_close_open'] + [metric+x for x in ['_Ret', '_Ret_MA_3', '_Ret_MA_15', '_Ret_MA_45', '_MTD', '_YTD']]
 
@@ -34,17 +45,44 @@ def run_sklearn_model(model, train, test, features, target):
     except:
         pass
     y_pred = reg.predict(X_test)
-    print("MSE: ", mean_squared_error(y_test, y_pred))
-    print("R2: ", r2_score(y_test, y_pred))
-    plot_results(y_test, y_pred, model)
+    return({"MSE":mean_squared_error(y_test, y_pred),
+            "R2" :r2_score(y_test, y_pred)})
+    # print("MSE: ", mean_squared_error(y_test, y_pred))
+    # print("R2: ", r2_score(y_test, y_pred))
+    # plot_results(y_test, y_pred, model)
 
 
-def do_stuff(cur, model):
-    forex_cols = [x for x in forex.columns if x[1] == cur[0]]
-    index_cols = [x for x in index.columns if x[1] == cur[0] and x[2] == cur[1]]
+def do_forex(cur, model, transf = False):
+    forex_cols = [x for x in forex.columns if x[1] == cur]
     X = forex[[col for col in forex_cols if col[0] in features + ['Time features']]][:-1]
     y = forex[[col for col in forex_cols if col[0] in target]].shift(-1)[:-1]
     X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=0.2)
-    run_sklearn_model(model, (X_train, y_train), (X_test, y_test), features, 'ret')
+    res = run_sklearn_model(model, (X_train, y_train), (X_test, y_test), features, target)
+    return(res)
 
-do_stuff(["HKD", "Hang Seng"], linear_model.LinearRegression)
+def iterate_markets():
+    reg_res = {}
+    for f_m in forex_pairs:
+        for model in reg_models:
+            print(f_m, model)
+            try:
+                res = do_forex(f_m, model)
+            except:
+                pass
+            res['Forex pair'] = f_m
+            reg_res[model().__class__.__name__] = res
+    try:
+        reg = pd.read_json(json.loads(reg_res), orient='index')
+        print(reg)
+        return(reg)
+    except:
+        return(reg_res)
+
+res = iterate_markets()
+res_df = pd.DataFrame(res, orient='index', columns= ['Forex pair', 'MSE', 'R2'])
+# for item in res:
+#     res_df = res_df.append(item[list(item.keys())[0]], ignore_index = True)
+# res_df
+# res_df.index = [list(x.keys())[0] for x in res]
+res_df.to_csv("Linear_model_results.csv")
+# do_stuff(["HKD", "Hang Seng"], LinearRegression)
