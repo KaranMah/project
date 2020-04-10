adbdf1a03687af8c680510f14c8da6fde006a5db
Karan M <karan22.mahajan@gmail.com>
2020-04-11 00:02:31 +0530

    backtesting basic impl

--- backtesting_strat.py
+++ backtesting_strat.py
@@ -1,7 +1,9 @@
-from backtesting import Strategy
-from backtesting.lib import crossover
+from backtesting import *
 from backtesting import Backtest
+from backtesting.lib import crossover
+from backtesting.test import SMA
 import pandas as pd
+from backtesting.test import GOOG
 
 
 class ClasStrat(Strategy):
@@ -25,14 +27,21 @@ class ClasStrat(Strategy):
         elif crossover(self.sma2, self.sma1):
             self.sell()
 
-def readData (start):
-    data = pd.read_csv(start+"results.csv").to_datetime() #confirm
+
+def readData (curr):
+    forex_features = ["Open", "Close", "High", "Low", "Volume"]
+    forex = pd.read_csv('prep_forex.csv', header=[0, 1], index_col=0)
+    forex_cols = [x for x in forex.columns if x[1] == curr]
+    data = forex[[col for col in forex_cols if col[0] in forex_features]][:-1]
+    data.columns = [x[0] for x in list(data.columns)]
+    data.index = pd.to_datetime(data.index)
+    print(data.head())
     return data
 
 strat = ["Classification", "Regressiont"] 
 
-data = readData(strat)
-bt = backtest(data, ClasStrat, cash = 10000, commision = 0.02)
+data = readData("INR")
+bt = Backtest(data, ClasStrat, cash=10000, commission=0.0001)
 res = bt.run()
 print(res)
 bt.plot()
