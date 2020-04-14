a6731cfb55e681fdd6ec340c578fbb55e650d98b
Karan M <karan22.mahajan@gmail.com>
2020-04-14 18:48:16 +0530

    updated walk_forward

--- walk_forward_classification.py
+++ walk_forward_classification.py
@@ -8,6 +8,7 @@ import warnings
 from sklearn.exceptions import DataConversionWarning
 
 warnings.filterwarnings(action='ignore', category=DataConversionWarning)
+warnings.filterwarnings(action='ignore', category=UserWarning)
 
 forex = pd.read_csv('prep_forex.csv', header=[0, 1], index_col=0)
 index = pd.read_csv('prep_index.csv', header=[0, 1, 2], index_col=0)
@@ -15,12 +16,21 @@ index = pd.read_csv('prep_index.csv', header=[0, 1, 2], index_col=0)
 forex_pairs = list(set([x[1] for x in forex.columns if x[0] == 'Close']))
 index_pairs = list(set([(x[1], x[2]) for x in index.columns if x[0] == 'Close']))
 
-cls_models = [RidgeClassifier]
+cls_models = [RidgeClassifierCV, RidgeClassifier]
 reg_models = [SVR, LinearSVR, LinearRegression]
 
-forex_feature = "VND"
-index_feature = ("MNT", "MNE Top 20")
+target_markets = [('PKR', 'Karachi 100'), ('LKR', 'CSE All-Share')]
+features = {"MNT": [None, "LKR", ("NZD", "NZX MidCap")],
+            ('PKR', 'Karachi 100'): [None, "INR", ('JPY', 'NIkkei 225')],
+            ('LKR', 'CSE All-Share'): [None, "IDR", ('MNT', 'MNE Top 20')],
+            "BDT": [("IDR", "IDX Composite"), None, "VND"]}
 
+# forex_feature = "VND"
+# index_feature = ("BDT", "DSE 30")
+
+accuracy_lst = pd.DataFrame()
+csv_name = ""
+csv_dir = "./walk_forward_sliding_cross_results/"
 
 metric = 'Close'
 metrics = ['Open', 'Close', 'Low', 'High']
@@ -33,9 +43,10 @@ index_features = ['Intraday_HL', 'Intraday_OC', 'Prev_close_open'] + [y + x for 
                                                                       ['_Ret', '_Ret_MA_3', '_Ret_MA_15', '_Ret_MA_45',
                                                                        '_MTD', '_YTD'] for y in (metrics + ['Volume'])]
 
-scalers = [MinMaxScaler, StandardScaler, FunctionTransformer]
+# scalers = [None, MinMaxScaler, StandardScaler]
+xstr = lambda s: '' if s is None else str(s)
 
-def run_sklearn_model(model, train, test, features, target):
+def run_sklearn_model(model, train, test, feat, target):
     X_train, y_train = train
     X_test, y_test = test
     X_train = pd.DataFrame(X_train)
@@ -46,6 +57,7 @@ def run_sklearn_model(model, train, test, features, target):
     data = X_train.values
     data_y = y_train
     for i, t in enumerate(X_test.values):
+        global accuracy_lst
         reg = model()
         reg.fit(data, data_y)
         if i == 0:
@@ -60,21 +72,24 @@ def run_sklearn_model(model, train, test, features, target):
         data_y = np.delete(data_y, 0, 0)
     y_true = np.vstack((y_train, y_test))
     prediction = pd.DataFrame(prediction)
-    print(model.__name__ + " " + str(period) + " accuracy = ", accuracy_score(y_true, prediction))
-    print(confusion_matrix(y_true, prediction))
+
+    acc = accuracy_score(y_true, prediction)
+    print(model.__name__ + " accurcy for "+ xstr(feat) + " with period "+ str(period)+"="+str(acc))
+    class_report = classification_report(y_true, prediction, digits=2)
+    data = np.array([class_report])
+    acc_df = pd.DataFrame(data=data)
+    accuracy_lst = pd.concat([accuracy_lst, acc_df])
+
     x_true = pd.concat([X_train, X_test], axis=0)
     prediction.index = x_true.index
     return prediction
 
-
 def split_scale(X, y, scaler, train_index, test_index, shuffle=False, poly=False):
     X_train, X_test = X.iloc[:train_index], X.iloc[train_index:]
     y_train, y_test = y.iloc[:train_index], y.iloc[train_index:]
-
-    if scaler:
+    if scaler is not None:
         scaler_X = scaler()
         if(scaler == FunctionTransformer):
-            print("hi")
             scaler_X = scaler(np.log1p)
         scaler_X = scaler_X.fit(X_train)
         X_train = scaler_X.transform(X_train)
@@ -89,71 +104,88 @@ def split_scale(X, y, scaler, train_index, test_index, shuffle=False, poly=False
     y_test[y_test == 0] = -1
     y_test = np.array(y_test).reshape(-1, 1)
     y_train = np.array(y_train).reshape(-1, 1)
-    print(pd.DataFrame(X_test).shape)
     return (X_train, X_test, y_train, y_test)
 
 
-def add_cross_domain_features(f_feat = forex_feature, i_feat = index_feature):
-    index_cols = index_cols = [x for x in index.columns if x[1] == i_feat[0] and x[2] == i_feat[1]]
-    X_i = index[[col for col in index_cols if col[0] in index_features + ['Time features']]][:-1]
-    forex_cols = [x for x in forex.columns if x[1] == f_feat]
-    X_f = forex[[col for col in forex_cols if col[0] in forex_features + ['Time features']]][:-1]
-    cd_feats = pd.concat([X_i, X_f], axis=1)
-    return(cd_feats)
+def add_cross_domain_features(feat):
+    if(isinstance(feat, tuple)):
+        index_cols = index_cols = [x for x in index.columns if x[1] == feat[0] and x[2] == feat[1]]
+        X = index[[col for col in index_cols if col[0] in index_features + ['Time features']]][:-1]
+    else:
+        forex_cols = [x for x in forex.columns if x[1] == feat]
+        X = forex[[col for col in forex_cols if col[0] in forex_features + ['Time features']]][:-1]
+    return(X)
 
-def do_forex(cur, model, train_index, test_index, transf=None, shuffle=False, poly=False, transf_features_also=False):
+def do_forex(cur, model, train_index, test_index, feat, transf=None, shuffle=False, poly=False):
     forex_cols = [x for x in forex.columns if x[1] == cur]
     X = forex[[col for col in forex_cols if col[0] in forex_features + ['Time features']]][:-1]
-    X = X.join(add_cross_domain_features())
+    if feat:
+        X = X.join(add_cross_domain_features(feat))
+
     y = forex[[col for col in forex_cols if col[0] in target]].shift(-1)[:-1]
+    X = X.dropna(how='all', axis=1)
     X = X.dropna(how='any')
     y = y[y.index.isin(X.index)]
+
     X_train, X_test, y_train, y_test = split_scale(X, y, transf, train_index, test_index, shuffle, poly)
-    res = run_sklearn_model(model, (X_train, y_train), (X_test, y_test), forex_features, target)
-    return (res)
+    res = run_sklearn_model(model, (X_train, y_train), (X_test, y_test), feat, target)
+    return res
 
 
-def do_index(cur, model, train_index, test_index, transf=None, shuffle=False, poly=False, transf_features_also=False):
+def do_index(cur, model, train_index, test_index, feat, transf=None, shuffle=False, poly=False):
     index_cols = [x for x in index.columns if x[1] == cur[0] and x[2] == cur[1]]
     X = index[[col for col in index_cols if col[0] in index_features + ['Time features']]][:-1]
-    X = X.join(add_cross_domain_features())
+    if feat:
+        X = X.join(add_cross_domain_features(feat))
     y = index[[col for col in index_cols if col[0] in target]].shift(-1)[:-1]
     X = X.dropna(how='any')
     y = y[y.index.isin(X.index)]
     X_train, X_test, y_train, y_test = split_scale(X, y, transf, train_index, test_index, shuffle, poly)
-    res = run_sklearn_model(model, (X_train, y_train), (X_test, y_test), index_features, target)
+    res = run_sklearn_model(model, (X_train, y_train), (X_test, y_test), feat, target)
     return (res)
 
 
 def iterate_markets():
     shuffle = False
     poly = False
-    reg_res = pd.DataFrame()
-    for f_m in ["BDT"]:
+    global accuracy_lst
+    for f_m in target_markets:
         print(f_m)
         for model in cls_models:
-            for scaler in scalers:
-                for i in range(30, 35, 5):
-                    if (f_m in forex_pairs):
-                        train_index = i
-                        test_index = 0
-                        res = do_forex(f_m, model, train_index, test_index, scaler, shuffle, poly)
-                    else:
-                        train_index = int(len(index) * .8)
-                        test_index = 0
-                        res = do_index(f_m, model, train_index, test_index, scaler, shuffle, poly)
-                    if scaler:
-                        res.columns = [scaler.__name__ + "_pred_" + str(i)]
-                    else:
-                        res.columns = ["None_pred_" + str(i)]
+            print(model.__name__)
+            for scaler in [None]:
+                for feat in features[f_m]:
+                    print(feat)
+                    reg_res = pd.DataFrame()
+                    rows = []
+                    for i in range(5, 55, 5):
+                        try:
+                            if (f_m in forex_pairs):
+                                train_index = i
+                                test_index = 0
+                                res = do_forex(f_m, model, train_index, test_index, feat, scaler, shuffle, poly)
+                            else:
+                                train_index = i
+                                test_index = 0
+                                res = do_index(f_m, model, train_index, test_index, feat, scaler, shuffle, poly)
+                            if isinstance(feat, tuple):
+                                res.columns = [feat[0] + "_pred_" + str(i)]
+                            else:
+                                res.columns = [xstr(feat) + "_pred_" + str(i)]
 
-                    print(reg_res.shape, res.shape)
-                    # reg_res = reg_res.append(res)
-                    reg_res = pd.concat([reg_res, res], axis=1)
+                            reg_res = pd.concat([reg_res, res], axis=1)
+                            rows.append(i)
+                        except Exception as e:
+                            print(e)
+                            pass
 
-                    # except Exception as e:
-                    #     print(e)
-            reg_res.to_csv(str(f_m) + "_" + model.__name__ + "_sliding_sclaed_results.csv")
+                    csv_name = csv_dir + str(f_m) + "_" + model.__name__ + "_" + xstr(feat)
+                    reg_res.to_csv(csv_name+".csv")
+                    accuracy_lst.index = rows
+                    accuracy_lst.columns = ["report"]
+                    accuracy_lst.to_csv(csv_name + "_metrics.csv")
+                    accuracy_lst = pd.DataFrame()
 
 
 iterate_markets()
+
