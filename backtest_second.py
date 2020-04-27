57ffaa8eb7c3f2935e5d784a2216963476955352
Karan M <karan22.mahajan@gmail.com>
2020-04-27 18:16:31 +0530

    shifting to server

--- backtest_second.py
+++ backtest_second.py
@@ -1,12 +1,10 @@
-import glob
-import re
 from backtesting import *
 from backtesting import Backtest
-from backtesting.lib import crossover
+from backtesting.lib import crossover, plot_heatmaps
 from backtesting.test import SMA
 import pandas as pd
 import numpy as np
-from sklearn.svm import SVC
+import seaborn
 
 forex = pd.read_csv('prep_forex.csv', header=[0, 1], index_col=0)
 index = pd.read_csv('prep_index.csv', header=[0, 1, 2], index_col=0)
@@ -14,10 +12,8 @@ index = pd.read_csv('prep_index.csv', header=[0, 1, 2], index_col=0)
 forex_pairs = list(set([x[1] for x in forex.columns if x[0] == 'Close']))
 index_pairs = list(set([(x[1], x[2]) for x in index.columns if x[0] == 'Close']))
 
-cls_models = [SVC]
-
-files = glob.glob("*_SVC_sliding*")
-
+csv_dir = "./walk_forward_opt/final/"
+xstr = lambda s: '' if s is None else str(s)
 
 def majority(pred, n=4):
     res = pred.rolling(n).sum()
@@ -34,10 +30,9 @@ class ClasStrat(Strategy):
     n2 = 60
 
     def init(self):
-        # Precompute two moving averages
-        pred = pd.read_csv(cur + "_Close_prophet.csv")
-        # res = iterate_markets(self.i)
-        res = pred[['pred']]
+        pred = pd.read_csv(csv_dir + cur + "_final" + xstr(win) + ".csv")
+        res = pred[['pred_mode']]
+        res = res[int(len(res)*.8):]
         maj = np.array(majority(res, self.window)).ravel()
         self.sma1 = self.I(SMA, maj, self.n1)
         self.sma2 = self.I(SMA, maj, self.n2)
@@ -58,21 +53,32 @@ def readData(curr):
     data.index = pd.to_datetime(data.index)
     return data
 
+target_markets = ['BDT', 'MNT', ('PKR', 'Karachi 100'), ('LKR', 'CSE All-Share')]
+window = {"MNT": [None, "_40"],
+          ('PKR', 'Karachi 100'): [None, "_45"],
+          ('LKR', 'CSE All-Share'): [None, "_30"],
+          "BDT": [None, "_50"]}
 
 forex_pairs.sort()
-for f_m in ["BDT"]:
+for f_m in target_markets:
     cur = f_m
     print(cur)
-    data = readData(cur)
-    data = data[int(len(data)*.8):]
-    bt = Backtest(data, ClasStrat, cash=10000, commission=0.002)
-    res = bt.run()
-    print(res)
-    bt.plot()
-    # stats = bt.optimize(window=range(2, 10, 1),
-    #                     i=range(lower_lim, upper_lim, 5),
-    #                     n1=range(5, 30, 5),
-    #                     n2=range(20, 80, 5),
-    #                     maximize='Equity Final [$]',
-    #                     constraint=lambda p: p.n1 < p.n2)
-    # print(stats)
+    for win in window[f_m]:
+        data = readData(cur)
+        data = data[int(len(data)*.8):]
+        bt = Backtest(data, ClasStrat, cash=100000, commission=0.002, trade_on_close=True)
+        res = bt.run()
+        # print(res)
+        fileName = "./images/bt_plot/"+cur + xstr(win)
+        bt.plot(filename=fileName)
+        stats, heat_map = bt.optimize(window=range(2, 10, 1),
+                        n1=range(5, 30, 5),
+                        n2=range(20, 80, 5),
+                        maximize='Equity Final [$]',
+                        constraint=lambda p: p.n1 < p.n2,
+                        return_heatmap=True)
+
+        fileName = "./images/bt_plot/heatmap_" + cur + xstr(win)
+        plot_heatmaps(heatmap=heat_map, filename=fileName+"_heatmap")
+        print(cur + xstr(win))
+        print(stats)
